78f74a279dc17029f8ef8081ca2e57ee0b6d285f
Matt Kruse <github@mattkruse.com>
2021-05-07 23:04:05 -0500

    Match Runner POC

--- docs/match-runner/script.js
+++ docs/match-runner/script.js
@@ -1,3 +1,5 @@
+let results = null;
+
 async function postData(url = '', data = {}) {
   try {
     const response = await fetch(url, {
@@ -30,26 +32,33 @@ async function play() {
   let url = 'https://u54uysdcqj.execute-api.us-east-1.amazonaws.com/dev/match';
   console.log( JSON.stringify(req));
   try {
-    let results = await postData(url, req);
+    results = await postData(url, req);
     document.querySelector('#results').textContent = JSON.stringify(results,null,2);
 
-    let states = [];
-    results.state.forEach(gs=>{
-      gs.forEach(s=>{
-        states.push(s);
-      })
-    });
-    let showNextState = function() {
-      if (states.length) {
-        let s = states.shift();
-        document.querySelector('#game_render').contentWindow.postMessage(s,"*");
-      }
-      setTimeout(showNextState,200);
-    };
-    showNextState();
+    let tally = results.results;
+    let list = document.querySelector('#game_list');
+    list.innerHTML = '';
+
+    for (let i=0;i<tally.gameWinners.length;i++) {
+      list.innerHTML += `<div class="game_entry" onclick="showgame(${i},this)" style="cursor:pointer;height:30px;border:1px solid gray;margin:3px;">Game #${i+1} Winner: ${tally.gameWinners[i].join(',')}</div>`;
+    }
+
+    // Show the first game by default
+    showgame(0);
 
   } catch(e) {
     console.log(e);
   }
+}
 
+function showgame(i) {
+  let selected = document.querySelector('.game_selected');
+  if (selected) {
+    selected.classList.remove('game_selected');
+  }
+  document.querySelector('#game_list').childNodes[i].classList.add('game_selected');
+
+  let gamestates = results.state[i];
+  let finalstate = gamestates[gamestates.length-1];
+  document.querySelector('#game_render').contentWindow.postMessage(finalstate,"*");
 }
