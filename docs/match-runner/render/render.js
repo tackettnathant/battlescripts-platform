bd746e1bc8d12969eb28efd7b39e9425386e2774
Matt Kruse <github@mattkruse.com>
2021-05-08 07:45:39 -0500

    Update to Match Runner POC

--- docs/match-runner/render/render.js
+++ docs/match-runner/render/render.js
@@ -1,13 +1,21 @@
 function render(state) {
-  let sq = s=>{
+  let sq = (row,col)=>{
+    let s = state.board[row][col];
     let t = (s===null?"&nbsp;": s==="0"?"X": "O");
-    return `<td class="${s===null?'':t}">${t}</td>`;
+    let className = '';
+    let w = state.winning_moves;
+    if (w) {
+      if (w[0][0]==row && w[0][1]==col) { className="win"; }
+      if (w[1][0]==row && w[1][1]==col) { className="win"; }
+      if (w[2][0]==row && w[2][1]==col) { className="win"; }
+    }
+    return `<td class="${s===null?'':t} ${className}">${t}</td>`;
   };
   let b = state.board;
   let html = `<table id="board">
-      <tr> ${sq(b[0][0])} ${sq(b[0][1])} ${sq(b[0][2])} </tr>
-      <tr> ${sq(b[1][0])} ${sq(b[1][1])} ${sq(b[1][2])} </tr>
-      <tr> ${sq(b[2][0])} ${sq(b[2][1])} ${sq(b[2][2])} </tr>
+      <tr> ${sq(0,0)} ${sq(0,1)} ${sq(0,2)} </tr>
+      <tr> ${sq(1,0)} ${sq(1,1)} ${sq(1,2)} </tr>
+      <tr> ${sq(2,0)} ${sq(2,1)} ${sq(2,2)} </tr>
     </table>`;
   document.body.innerHTML = html;
 }
--- docs/match-runner/script.js
+++ docs/match-runner/script.js
@@ -23,10 +23,12 @@ async function postData(url = '', data = {}) {
 async function play() {
   let p1 = document.querySelector('#p1').value;
   let p2 = document.querySelector('#p2').value;
+  let game = document.querySelector('#game').value;
   let games = document.querySelector('#games').value;
   let req = {
     "p1":p1,
     "p2":p2,
+    "game":game,
     "games":games
   };
   let url = 'https://u54uysdcqj.execute-api.us-east-1.amazonaws.com/dev/match';
@@ -43,6 +45,17 @@ async function play() {
       list.innerHTML += `<div class="game_entry" onclick="showgame(${i},this)" style="cursor:pointer;height:30px;border:1px solid gray;margin:3px;">Game #${i+1} Winner: ${tally.gameWinners[i].join(',')}</div>`;
     }
 
+    let winner = tally.winners.length==2 ? "It's a tie!" :
+      tally.winners[0] == "0" ? "Player #1" : "Player #2";
+    list.innerHTML += `
+      <div>Match Winner: ${winner}</div>
+    `;
+
+    list.innerHTML += `
+      <input type="button" value="&#x23ee;" onclick="back()">
+      <input type="button" value="&#9654;" onclick="playgame()">
+      <input type="button" value="&#x23ed;" onclick="forward()">
+    `;
     // Show the first game by default
     showgame(0);
 
@@ -51,7 +64,11 @@ async function play() {
   }
 }
 
+let selected_game = 0;
+let currentstate = 0;
+
 function showgame(i) {
+  selected_game = i;
   let selected = document.querySelector('.game_selected');
   if (selected) {
     selected.classList.remove('game_selected');
@@ -60,5 +77,45 @@ function showgame(i) {
 
   let gamestates = results.state[i];
   let finalstate = gamestates[gamestates.length-1];
+  currentstate = gamestates.length-1;
   document.querySelector('#game_render').contentWindow.postMessage(finalstate,"*");
+}
+function showcurrentstate() {
+  let gamestates = results.state[selected_game] || [];
+  let s = gamestates[currentstate];
+  if (s) {
+    document.querySelector('#game_render').contentWindow.postMessage(s, "*");
+  }
+}
+function playgame() {
+  let gamestates = results.state[selected_game];
+  currentstate = 0;
+  let show = ()=> {
+    let s = gamestates[currentstate];
+    if (s) {
+      document.querySelector('#game_render').contentWindow.postMessage(s, "*");
+      currentstate++;
+      setTimeout(show, 500);
+    }
+    else {
+      currentstate--; // reached end
+    }
+  };
+  show();
+}
+function back() {
+  currentstate--;
+  if (currentstate<0) {
+    currentstate = 0;
+  }
+  showcurrentstate();
+}
+function forward() {
+  let gamestates = results.state[selected_game];
+  currentstate++;
+  if (currentstate>gamestates.length-1) {
+    currentstate = gamestates.length-1;
+  }
+  showcurrentstate();
+
 }
